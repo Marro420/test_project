776189c8912b0a3beda1bf0e3cc7d9eec6106d1f
Marro420 <mtmuxa@mail.ru>
2020-04-22 20:28:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.menu__btn').on('click', function () {
+   $('.menu').toggleClass('menu__active');
+})
+$('.menu__btn').on('click', function () {
+   $('.wrapper__background').toggleClass('wrapper__background_active');
+})
